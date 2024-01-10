ddb40e98076af92a06a47bcfcf00e23727d61890
Phumuza Sibisi <109278974+sibisiphumuza@users.noreply.github.com>
2024-01-10 09:00:51 +0200

    Full Commit.
    Full commit of End-to-end Selenium Java Framework Basic, step-by-step How to Guide.
    
    Blogpost.

---
+++ src/test/java/testcases/FirstTestcase.java
@@ -0,0 +1,31 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import base.TestBase;
+
+public class FirstTestcase extends TestBase {
+
+	@Test
+	
+	public static void ReadMeTest()
+	{
+		// TODO Auto-generated method stub		
+		
+		WebElement intro = driver.findElement(By.xpath(locators.getProperty("intro")));
+		WebElement remember = driver.findElement(By.xpath(locators.getProperty("remember")));
+		WebElement tools = driver.findElement(By.xpath(locators.getProperty("tools")));
+		WebElement connect = driver.findElement(By.xpath(locators.getProperty("connect")));
+		WebElement repos = driver.findElement(By.xpath(locators.getProperty("repos")));
+		
+		System.out.println(intro.getText());
+		System.out.println(remember.getText());
+		System.out.println(tools.getText());
+		System.out.println(connect.getText());
+		System.out.println(repos.getText());
+
+	}
+
+}
